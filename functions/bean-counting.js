b62821dfcbf825ab7daf1478f3880959a3ec14cf
Ian Hundere <ianfundere@gmail.com>
2019-01-02 09:04:35 -0500

    adds function exercises

---
+++ functions/bean-counting.js
@@ -0,0 +1,17 @@
+const countChar = (string, ch) => {
+	let counted = 0;
+	for (let i = 0; i < string.length; i++) {
+		if (string[i] === ch) {
+			counted += 1;
+		}
+	}
+	return counted;
+};
+
+const countBs = (string) => {
+	return countChar(string, 'B');
+};
+
+console.log(countBs('BBC'));
+
+console.log(countChar('kakkerlaker', 'k'));
---
+++ functions/minimum.js
@@ -0,0 +1,7 @@
+const min = (a, b) => {
+	if (a < b) return a;
+	else return b;
+};
+
+console.log(min(-1, -2));
+console.log(min(1, 8));
